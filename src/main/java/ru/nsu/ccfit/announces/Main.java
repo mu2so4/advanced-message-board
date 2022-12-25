3195802652a4077cceea8a521665b7899ae18a26
mu2so4 <b4.orangutang@gmail.com>
2022-12-25 13:51:44 +0700

    Connect project to PostgreSQL

--- src/main/java/ru/nsu/ccfit/announces/Main.java
+++ src/main/java/ru/nsu/ccfit/announces/Main.java
@@ -1,7 +1,18 @@
 package ru.nsu.ccfit.announces;
-
+import java.sql.*;
 public class Main {
     public static void main(String[] args) {
-
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/announces",
+                            "announceDB", "12121212");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("Database opened successfully");
     }
 }
