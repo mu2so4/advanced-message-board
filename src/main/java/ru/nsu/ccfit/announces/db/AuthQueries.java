e22aeb8b5ccf6ad72f224f26d6dd2906950c24b3
mu2so4 <b4.orangutang@gmail.com>
2022-12-26 01:10:44 +0700

    Добавил добавление токенов в БД

--- src/main/java/ru/nsu/ccfit/announces/db/AuthQueries.java
+++ src/main/java/ru/nsu/ccfit/announces/db/AuthQueries.java
@@ -1,6 +1,7 @@
 package ru.nsu.ccfit.announces.db;
 
 import java.security.SecureRandom;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +11,8 @@ public class AuthQueries {
     private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
     private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
     public static String checkCredentials(String login, String password) throws SQLException {
-        PreparedStatement statement = AnnounceDB.getInstance().getConnection().
+        Connection connection = AnnounceDB.getInstance().getConnection();
+        PreparedStatement statement = connection.
                 prepareStatement("SELECT user_id FROM \"Users\" WHERE \"login\"= ? AND \"password\" = ? ;");
         statement.setString(1, login);
         statement.setString(2, password);
@@ -18,8 +20,17 @@ public class AuthQueries {
         if(!set.next()) {
             return null;
         }
+        int userId = set.getInt("user_id");
         byte[] randomBytes = new byte[24];
         secureRandom.nextBytes(randomBytes);
-        return base64Encoder.encodeToString(randomBytes);
+        String token = base64Encoder.encodeToString(randomBytes);
+
+        PreparedStatement hashStatement = connection.
+                prepareStatement("INSERT INTO \"AuthTokens\"(token, user_id, expiration_time)" +
+                        "VALUES(?, ?, NOW() + INTERVAL '10 min');");
+        hashStatement.setString(1, token);
+        hashStatement.setInt(2, userId);
+        hashStatement.executeUpdate();
+        return token;
     }
 }
