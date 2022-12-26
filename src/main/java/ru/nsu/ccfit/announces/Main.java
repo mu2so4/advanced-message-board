3dac8d6713bdd263e84e8fd14c24581e11866d4b
mu2so4 <b4.orangutang@gmail.com>
2022-12-26 07:52:08 +0700

    Добавил добавление в предложку

--- src/main/java/ru/nsu/ccfit/announces/Main.java
+++ src/main/java/ru/nsu/ccfit/announces/Main.java
@@ -1,25 +1,22 @@
 package ru.nsu.ccfit.announces;
 import ru.nsu.ccfit.announces.db.AnnounceDB;
-import ru.nsu.ccfit.announces.db.AuthQueries;
-import ru.nsu.ccfit.announces.db.SqlModifiers;
-import ru.nsu.ccfit.announces.db.SqlQueries;
+import ru.nsu.ccfit.announces.db.AnnounceQueries;
+import ru.nsu.ccfit.announces.db.auth.AuthQueries;
 
-import java.sql.ResultSet;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String login = scanner.nextLine();
-        String password = scanner.nextLine();
+        String text = scanner.nextLine();
+        int count = scanner.nextInt();
+        Integer[] ids = new Integer[count];
+        for(int index = 0; index < count; index++) {
+            ids[index] = scanner.nextInt();
+        }
         scanner.close();
         try (AnnounceDB db = AnnounceDB.getInstance()) {
-            String token = AuthQueries.checkCredentials(login, password);
-            int id = AuthQueries.checkToken(token);
-            System.out.println(id);
-            System.out.println(token);
-            AuthQueries.clearExpiredTokens();
+            AnnounceQueries.submitAnnounceSuggestion(text, ids);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/ru/nsu/ccfit/announces/db/AnnounceQueries.java
+++ src/main/java/ru/nsu/ccfit/announces/db/AnnounceQueries.java
@@ -1,16 +1,12 @@
 package ru.nsu.ccfit.announces.db;
 
-import ru.nsu.ccfit.announces.subject.Subject;
-import ru.nsu.ccfit.announces.subject.SubjectReader;
+import ru.nsu.ccfit.announces.db.auth.AuthQueries;
+import ru.nsu.ccfit.announces.db.auth.AuthenticationException;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.*;
 
-public final class SqlModifiers {
-    private SqlModifiers() {}
+public final class AnnounceQueries {
+    private AnnounceQueries() {}
 
     public static void addSubject(String name) throws SQLException {
         PreparedStatement statement = AnnounceDB.getInstance().getConnection().
@@ -19,5 +15,41 @@ public final class SqlModifiers {
         statement.executeUpdate();
     }
 
+
+    public static void submitAnnounceSuggestion(String text, Integer[] subjectIds) throws SQLException {
+        String announceQuery =
+                """
+                INSERT
+                INTO "Announces" (
+                    text,
+                    create_time
+                )
+                VALUES (
+                    ?,
+                    NOW()
+                )
+                RETURNING announce_id;
+                """;
+        String announceSubjectsQuery =
+                """
+                INSERT
+                INTO "AnnounceSubjects" (announce_id, subject_id)
+                VALUES(?, UNNEST(?));
+                """;
+        Connection connection = AnnounceDB.getInstance().getConnection();
+        connection.setAutoCommit(false);
+        PreparedStatement announceStatement = connection.prepareStatement(announceQuery);
+        announceStatement.setString(1, text);
+        ResultSet resultId = announceStatement.executeQuery();
+        resultId.next();
+        int id = resultId.getInt("announce_id");
+        PreparedStatement subjectsStatement = connection.prepareStatement(announceSubjectsQuery);
+        Array array = connection.createArrayOf("integer", subjectIds);
+        subjectsStatement.setInt(1, id);
+        subjectsStatement.setArray(2, array);
+        subjectsStatement.executeUpdate();
+        connection.commit();
+        connection.setAutoCommit(true);
+    }
 
 }
